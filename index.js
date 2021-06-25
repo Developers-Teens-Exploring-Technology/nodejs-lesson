61803f89e9d5cc560f0699e698f05a75ae71171d
werner <elmer@werners-MacBook-Air.local>
2021-06-25 01:45:06 -0700

    included post method via button on /home

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 //declare our server 
 const app = express(); 
-const port = 4200; 
+const port = 3200; 
+
 
 app.get('/', function(req,res){
-
     res.send("HELLO BOYS!");
 });
+
 app.get('/home',function(req, res){
     res.sendFile(__dirname + '/index.html');
 })
-
-app.listen(port, ()=> {
+app.post('/home', function(req,res){
+    res.send("POST() METHOD ACTIVATED");
+})
+app.listen(port, function(){
     console.log('Listening on port: ' + port);
 });
+
